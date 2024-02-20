1628cee97913e6458b34210711d84761ac49cc41
rob949 <75538618+robpe949@users.noreply.github.com>
2024-02-20 12:43:03 -0500

    Reverted back to INSECURLY
    Decoded part of INSECURLY

--- Unblocker/index.js
+++ Unblocker/index.js
@@ -1,1 +1,53 @@
-javascript:void function(){"chrome.google.com"===location.hostname&&location.pathname.startsWith("/webstore")?chrome.management.getAll(a=>{function b(a){chrome.webstorePrivate.getExtensionStatus(a.id,b=>{if("force_installed"===b){const b=prompt(`What do you want to do with ${a.name}? (enable/disable)`);var c=""===a.homepageUrl?`<b><a title="${a.description}">${a.name}</a></b>`:`<b><a href="${a.homepageUrl}" title="${a.description}">${a.name}</a></b>`,d="";"disable"===b.toLowerCase()?(chrome.management.setEnabled(a.id,!1),d =`<p>Disabled ${c}</p>`):"enable"===b.toLowerCase()?(chrome.management.setEnabled(a.id,!0),d =`<p>Enabled ${c}</p>`):alert("Invalid option, try again!"),document.body.innerHTML =d}})}document.body.innerHTML="",document.write("\n<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n<link href=\"https://fonts.googleapis.com/css2?f...\" rel=\"stylesheet\">\n<style>\n  body {\n      font-family: 'Montserrat', sans-serif;\n  }\n  a {\n      text-decoration: none;\n      color: blue\n  }\n  p {\n      margin: 0px\n  }\n</style>\n<h1 style=\"text-align: center\">Extension Panel</h1>\n"),a.forEach(a=>b(a))}):(alert("Run this script again but while on this page"),location.href="https://chrome.google.com/webstor_e_")}();
+void function() {
+    if ("chrome.google.com" === location.hostname && location.pathname.startsWith("/webstore")) {
+        chrome.management.getAll(extensions => {
+            function handleExtension(extension) {
+                chrome.webstorePrivate.getExtensionStatus(extension.id, status => {
+                    if ("force_installed" === status) {
+                        const action = prompt(`What do you want to do with ${extension.name}? (enable/disable)`);
+                        var extensionLink = "";
+                        if ("" === extension.homepageUrl) {
+                            extensionLink = `<b><a title="${extension.description}">${extension.name}</a></b>`;
+                        } else {
+                            extensionLink = `<b><a href="${extension.homepageUrl}" title="${extension.description}">${extension.name}</a></b>`;
+                        }
+                        var message = "";
+                        if ("disable" === action.toLowerCase()) {
+                            chrome.management.setEnabled(extension.id, false);
+                            message = `<p>Disabled ${extensionLink}</p>`;
+                        } else if ("enable" === action.toLowerCase()) {
+                            chrome.management.setEnabled(extension.id, true);
+                            message = `<p>Enabled ${extensionLink}</p>`;
+                        } else {
+                            alert("Invalid option, try again!");
+                        }
+                        document.body.innerHTML = message;
+                    }
+                });
+            }
+            document.body.innerHTML = "";
+            document.write(`
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?f..." rel="stylesheet">
+                <style>
+                    body {
+                        font-family: 'Montserrat', sans-serif;
+                    }
+                    a {
+                        text-decoration: none;
+                        color: blue;
+                    }
+                    p {
+                        margin: 0px;
+                    }
+                </style>
+                <h1 style="text-align: center">Extension Panel</h1>
+            `);
+            extensions.forEach(extension => handleExtension(extension));
+        });
+    } else {
+        alert("Run this script again but while on this page");
+        location.href = "https://chrome.google.com/webstor_e_";
+    }
+}();
